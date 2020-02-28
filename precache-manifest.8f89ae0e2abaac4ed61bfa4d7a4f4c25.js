e60fbb371a6b6220d503a9883c8109671fb9bab3
Igor <kapirotohat@gmail.com>
2020-02-28 00:42:01 -0300

    Updates

---
+++ precache-manifest.8f89ae0e2abaac4ed61bfa4d7a4f4c25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e84c7692cfb2d1b22edfda28eee309d",
+    "url": "/prototipo/index.html"
+  },
+  {
+    "revision": "28131cd5f86350274430",
+    "url": "/prototipo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad8c5bc3bbd8374c630e",
+    "url": "/prototipo/static/js/2.440ee2e5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/prototipo/static/js/2.440ee2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28131cd5f86350274430",
+    "url": "/prototipo/static/js/main.c9d11724.chunk.js"
+  },
+  {
+    "revision": "21f762bcc68cdecf58dc",
+    "url": "/prototipo/static/js/runtime-main.592ccb47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prototipo/static/media/logo.5d5d9eef.svg"
+  }
+]);
